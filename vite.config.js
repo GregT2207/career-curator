46f79e573d8b505e603d55e2417470ce3e0c437f
Greg <gregory.tuck@hotmail.com>
2023-05-20 22:48:12 +0100

    Scaffold frontend with Vue and router

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-
+import vue from '@vitejs/plugin-vue'
 export default defineConfig({
     plugins: [
+        vue(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+    server: {
+        hmr: {
+            protocol: "wss",
+            // if your DDEV project serves multiple hosts, update the "host" as required.
+            host: `${process.env.DDEV_HOSTNAME}`,
+        }
+    }
+
 });
